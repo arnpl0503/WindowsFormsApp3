73f43faed72a612483b7ec67ac4c27ebc8987d56
Aeron <Aeron Paul A. Amago@LAPTOP-PAMDIS9L>
2024-02-24 17:28:57 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MATH = new System.Windows.Forms.Label();
+            this.SCIENCE = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbtHistory = new System.Windows.Forms.TextBox();
+            this.tbtFilipino = new System.Windows.Forms.TextBox();
+            this.tbtEnglish = new System.Windows.Forms.TextBox();
+            this.tbtMath = new System.Windows.Forms.TextBox();
+            this.tbtScience = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grades";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "History";
+            // 
+            // MATH
+            // 
+            this.MATH.AutoSize = true;
+            this.MATH.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MATH.Location = new System.Drawing.Point(16, 94);
+            this.MATH.Name = "MATH";
+            this.MATH.Size = new System.Drawing.Size(59, 26);
+            this.MATH.TabIndex = 3;
+            this.MATH.Text = "Math";
+            // 
+            // SCIENCE
+            // 
+            this.SCIENCE.AutoSize = true;
+            this.SCIENCE.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SCIENCE.Location = new System.Drawing.Point(16, 131);
+            this.SCIENCE.Name = "SCIENCE";
+            this.SCIENCE.Size = new System.Drawing.Size(83, 26);
+            this.SCIENCE.TabIndex = 4;
+            this.SCIENCE.Text = "Science";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 26);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "English";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 26);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Filipino";
+            // 
+            // tbtHistory
+            // 
+            this.tbtHistory.Location = new System.Drawing.Point(105, 265);
+            this.tbtHistory.Name = "tbtHistory";
+            this.tbtHistory.Size = new System.Drawing.Size(100, 22);
+            this.tbtHistory.TabIndex = 7;
+            // 
+            // tbtFilipino
+            // 
+            this.tbtFilipino.Location = new System.Drawing.Point(105, 218);
+            this.tbtFilipino.Name = "tbtFilipino";
+            this.tbtFilipino.Size = new System.Drawing.Size(100, 22);
+            this.tbtFilipino.TabIndex = 8;
+            // 
+            // tbtEnglish
+            // 
+            this.tbtEnglish.Location = new System.Drawing.Point(105, 169);
+            this.tbtEnglish.Name = "tbtEnglish";
+            this.tbtEnglish.Size = new System.Drawing.Size(100, 22);
+            this.tbtEnglish.TabIndex = 9;
+            // 
+            // tbtMath
+            // 
+            this.tbtMath.Location = new System.Drawing.Point(105, 94);
+            this.tbtMath.Name = "tbtMath";
+            this.tbtMath.Size = new System.Drawing.Size(100, 22);
+            this.tbtMath.TabIndex = 10;
+            // 
+            // tbtScience
+            // 
+            this.tbtScience.Location = new System.Drawing.Point(105, 131);
+            this.tbtScience.Name = "tbtScience";
+            this.tbtScience.Size = new System.Drawing.Size(100, 22);
+            this.tbtScience.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(124, 13);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(293, 22);
+            this.textBox6.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(105, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 80);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Average";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbtTotal
+            // 
+            this.tbtTotal.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.tbtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbtTotal.Location = new System.Drawing.Point(350, 363);
+            this.tbtTotal.Name = "tbtTotal";
+            this.tbtTotal.Size = new System.Drawing.Size(200, 38);
+            this.tbtTotal.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbtTotal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.tbtScience);
+            this.Controls.Add(this.tbtMath);
+            this.Controls.Add(this.tbtEnglish);
+            this.Controls.Add(this.tbtFilipino);
+            this.Controls.Add(this.tbtHistory);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.SCIENCE);
+            this.Controls.Add(this.MATH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label MATH;
+        private System.Windows.Forms.Label SCIENCE;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbtHistory;
+        private System.Windows.Forms.TextBox tbtFilipino;
+        private System.Windows.Forms.TextBox tbtEnglish;
+        private System.Windows.Forms.TextBox tbtMath;
+        private System.Windows.Forms.TextBox tbtScience;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbtTotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mat, sci, eng, fil, his;
+            string total;
+
+            mat = int.Parse(tbtMath.Text);
+            sci = int.Parse(tbtScience.Text);
+            eng = int.Parse(tbtEnglish.Text);
+            fil = int.Parse(tbtFilipino.Text);
+            his = int.Parse(tbtHistory.Text);
+
+            double Total = (mat + sci + eng + fil + his) / 5;
+            tbtTotal.Text = Total.ToString();
+        }
+    }
+}
